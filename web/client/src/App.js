fd8be2315f1e68127a245c39d1f4867228b35050
andyliu-417 <andy2018.liu@gmail.com>
2018-05-20 14:29:41 -0400

    setState

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,9 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.state.count);
+    // this.setState(
+    //   prevState => ({
+    //     count: prevState.count + 1
+    //   }),
+    //   () => {
+    //     console.log(this.state.count);
+    //   }
+    // );
+    // console.log(this.state.count);
+
+    this.setState({count: this.state.count+1});
+    this.setState({count: this.state.count+1});
+
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  }
+
   render() {
+    console.log("render");
     return (
       <div className="App">
         <header className="App-header">
@@ -11,11 +43,12 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.count}
         </p>
       </div>
     );
   }
 }
+
 
 export default App;
